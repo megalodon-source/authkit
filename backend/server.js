29d00e82165babd840f522a097455ebffee49249
megalodon-source <dave117.id@gmail.com>
2024-10-17 03:04:19 +0700

    feat: server setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connect from "./src/db/connect.js";
 import cookieParser from "cookie-parser";
+import fs from "node:fs";
 
 dotenv.config();
 
@@ -19,8 +20,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+//routes
+const routeFiles = fs.readdirSync("./src/routes");
+
+routeFiles.forEach((file)=>{
+    // use dynamic import
+    import(`./src/routes/${file}`).then((route)=>{
+        app.use("/api/v1", route.default);
+    }).catch((err)=>{
+        console.log("Failed to load route file", err);
+    });
+});
+
 const server = async () => {
     try {
+
         await connect();
 
         app.listen(port, () => {
