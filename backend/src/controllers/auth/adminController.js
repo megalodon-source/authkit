148c3600689a4a5cee993615f5e9cf24d54a50f9
megalodon-source <dave117.id@gmail.com>
2024-10-19 20:25:09 +0700

    feat: get users

--- backend/src/controllers/auth/adminController.js
+++ backend/src/controllers/auth/adminController.js
@@ -16,3 +16,17 @@ export const deleteUser = asyncHandler(async (req, res) => {
     }
 });
 
+// get all users
+export const getAllUsers = asyncHandler(async (req, res) => {
+    try {
+        const users = await User.find({});
+
+        if (!users) {
+            res.status(404).json({ message: "No users found" });
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: "Cannot get users" });
+    }
+});
